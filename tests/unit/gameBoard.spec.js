67550cc59170fa0cb032f7d428bd2e4811b59596
Deyan Chakarov <dchakarov@shopmetrics.com>
2022-07-24 11:46:16 +0300

    tests for GameBoard Component: Default data props on first mount, Overflow visible when the matrix is not initialized, Hides overflow when child component emits init event

---
+++ tests/unit/gameBoard.spec.js
@@ -0,0 +1,34 @@
+import { mount } from "@vue/test-utils";
+import GameBoard from "@/components/GameBoard.vue";
+import UserControls from "@/components/UserControls.vue";
+
+describe("GameBoard.vue", () => {
+  let board;
+  beforeEach(() => {
+    board = mount(GameBoard);
+  });
+
+  it("Default data props on first mount", () => {
+    expect(board.vm.matrix).toHaveLength(0);
+    expect(board.vm.isMarrixCreated).toBe(false);
+    expect(board.vm.interval).toBe(null);
+    expect(board.vm.pattern).toEqual("");
+    expect(board.vm.isRunning).toBe(false);
+    expect(board.vm.totalAliveCells).toEqual(0);
+    expect(board.vm.totalCycles).toEqual(0);
+  });
+
+  it("Overflow visible when the matrix is not initialized", () => {
+    expect(board.findAll("#overlay")).toHaveLength(1);
+  });
+
+  it("Hides overflow when child component emits init event", async () => {
+    let controlsComponent = board.findComponent(UserControls);
+    await controlsComponent.vm.$emit("init", {
+      size: controlsComponent.vm.size,
+      pattern: controlsComponent.vm.pattern,
+    });
+
+    expect(board.find("#overlay").exists()).toBe(false);
+  });
+});
